3bb0b08c2512b4c2530ee30565606963d2c50732
njoroge33 <johngichuhi769@gmail.com>
2019-12-18 23:06:50 +0300

    create a signup view

--- instaphoto/views.py
+++ instaphoto/views.py
@@ -1,2 +1,12 @@
 from django.shortcuts import render
 
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'auth/signup.html', {'form': form})
+
