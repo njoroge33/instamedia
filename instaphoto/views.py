06e0788345f52c5f38eff077d542876086d7015a
njoroge33 <johngichuhi769@gmail.com>
2019-12-18 23:01:35 +0300

    setup db

--- instaphoto/views.py
+++ instaphoto/views.py
@@ -1,15 +1,2 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
-
-# Create your views here.
-
-def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login_url')
-    else:
-        form= UserCreationForm()
-    return render(request, 'auth/signup.html', {'form':form})
 
