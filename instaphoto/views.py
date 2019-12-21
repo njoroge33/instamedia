938647be512fa4d89751a1ed917189ba143d5bbf
njoroge33 <johngichuhi769@gmail.com>
2019-12-21 13:17:28 +0300

    style the signup page

--- instaphoto/views.py
+++ instaphoto/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .forms import SignUpForm
 
 def signup(request):
+    name = "Sign Up"
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
@@ -9,5 +10,5 @@ def signup(request):
         return redirect('home')
     else:
         form = SignUpForm()
-    return render(request, 'auth/signup.html', {'form': form})
+    return render(request, 'auth/signup.html', {'form': form, 'name':name})
 
