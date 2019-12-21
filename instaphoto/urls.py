8e3d63ee448a5dbc031d8eebe5813fde432a57f4
njoroge33 <johngichuhi769@gmail.com>
2019-12-21 23:21:09 +0300

    create a tmeline page

--- instaphoto/urls.py
+++ instaphoto/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('', views.signup, name='signup'),
+    re_path(r'^home/', views.index, name='home'),
     re_path(r'^login/', LoginView.as_view(), name='login_url'),
     re_path(r'^logout/', LogoutView.as_view(next_page='signup'), name='logout_url'),
 ]
--- instaphoto/views.py
+++ instaphoto/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .forms import SignUpForm
 
 def signup(request):
@@ -12,3 +13,7 @@ def signup(request):
         form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form, 'name':name})
 
+@login_required(login_url='/login/')
+def index(request):
+    current_user = request.user
+    return render(request, 'index.html', {'current_user':current_user})
