62dd118a3e873a5d3bd4b0d0fd1513ea2b7fbbf0
njoroge33 <johngichuhi769@gmail.com>
2020-01-05 16:31:04 +0300

    style the navbar html

--- instaphoto/urls.py
+++ instaphoto/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.signup, name='signup'),
     re_path(r'^home/', views.index, name='home'),
     re_path(r'^login/', LoginView.as_view(), name='login_url'),
-    re_path(r'^logout/', LogoutView.as_view(next_page='signup'), name='logout_url'),
+    re_path(r'^logout/', LogoutView.as_view(next_page='login_url'), name='logout_url'),
 ]
